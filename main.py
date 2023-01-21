2b0c59acb0753ffe213718ad2f8b74012d9ef92a
amrnasser <amrnasser92@gmail.com>
2023-01-21 20:38:09 +0200

    separated modules

--- main.py
+++ main.py
@@ -2,9 +2,14 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 import re
-import sqlalchemy
 import asyncio
-import psycopg2
+import sql
+import logging
+
+logging.basicConfig(level="INFO")
+logger = logging.getLogger()
+log = logging.FileHandler('log.txt','a')
+logger.addHandler(log)
 
 base_url = 'https://eg.hatla2ee.com'
 new_cars = []
@@ -14,53 +19,58 @@ used_cars_links = []
 existing_columns = []
 
 def get_links_new(page=1):
-    print(f'Extracting details from page {page}')
+    logging.info('Getting new links')
+    # logging.info(f'Extracting details from page {page}')
     url =f'https://eg.hatla2ee.com/en/new-car/page/{page}'
     r = requests.get(url)
+    logging.info(r.status_code)
     soup = BeautifulSoup(r.text,'lxml')
     links =  soup.find_all('a',class_='nCarListData_title')
     for link in links:
-        new_cars_links.append(base_url + link.attrs['href'])   
+        # new_cars_links.append(base_url + link.attrs['href'])   
+        full_link = base_url + link.attrs['href']
+        logging.info(full_link)
+        sql.insert_link('new_cars_links',link= full_link)
     next_page = soup.find_all('a',class_='paginate')[-1].attrs['href'].split("/")[-1]
     if int(next_page)>int(page):    
         get_links_new(next_page)    
     else:
-        print('No more pages')
-        print(f'Extracted {len(new_cars_links)} links')
+        logging.info('No more pages')
+        logging.info(f'Extracted {len(new_cars_links)} links')
 
 
 def get_links_used(page=1):
-    print(f'Extracting details from page {page}')
+    logging.info(f'Extracting details from page {page}')
     url =f'https://eg.hatla2ee.com/en/car/page/{page}'
     r = requests.get(url)
     soup = BeautifulSoup(r.text,'lxml')
     cars = soup.find_all('div', class_='newCarListUnit_header')
     for car in cars:
         link = car.find('a')
-        print(link.attrs['href'])
-        #used_cars_links.append(base_url + link.attrs['href'])
-        with open('usedcars.txt','a') as f:
-            f.write(base_url +link.attrs['href']+'\n')   
+        full_link = base_url + link.attrs['href']
+        logging.info(full_link)
+        sql.insert_link('used_cars_links',link=full_link)
+        # with open('usedcars.txt','a') as f:
+            # f.write(base_url +link.attrs['href']+'\n')   
     next_page = soup.find_all('a',class_='paginate')[-1].attrs['href'].split("/")[-1]
     if int(next_page)>int(page):    
         get_links_used(next_page)    
     else:
-        print('No more pages')
-        #print(f'Extracted {len(used_cars_links)} links')
+        logging.info('No more pages')
 
 
 def get_car_detail(url:str)->BeautifulSoup:
     r = requests.get(url)
-    print(url)
+    logging.info(url)
     return BeautifulSoup(r.text,'lxml'),url
 
 def extract_car_details_new(soup:BeautifulSoup)->None:
     if 'used' in soup.find('h1',class_='mainTitle').get_text().lower():
-        print('Used car')
+        logging.info('used car')
         return
     tables = soup.find('div',class_='newCarPricesWrap').find_all('tbody')
     car_name = soup.find('h2',class_='brandCarTitle').get_text().strip()
-    print(car_name)
+    logging.info(car_name)
     for table in tables:
         models = table.find_all('tr')
         for model in models:
@@ -78,7 +88,7 @@ def extract_car_details_new(soup:BeautifulSoup)->None:
                     'model': details[0].find('a').attrs['href'].split('/')[-2].title(),
                 }
                 new_cars.append(car)
-                insert_pgsql_table(car)
+                sql.insert_pgsql_table(car)
 
 
 def extract_car_details_used(url:str)->None:
@@ -108,177 +118,16 @@ def extract_car_details_used(url:str)->None:
         if detail.find(class_='DescDataSubTit'):
             new_key = detail.find(class_='DescDataSubTit').get_text().strip().replace(' ','_')
             if new_key not in existing_columns:
-                pgsql_add_column('used_cars',new_key)
+                sql.pgsql_add_column('used_cars',new_key)
                 existing_columns.append(new_key)
             car[detail.find(class_='DescDataSubTit').get_text().strip().lower()] = detail.find(class_='DescDataVal').get_text().strip()
-    print(car['make']," ", car['model'])
-    #used_cars.append(car)
-    insert_pgsql_table_used('used_cars',car)
-
-
-def create_pgsql_table_new_cars():
-    conn = psycopg2.connect(database='cars',user='postgres',password='Amr')
-    cursor=conn.cursor()
-    
-    SQL =f"""
-
-    CREATE TABLE IF NOT EXISTS new_cars(
-        id VARCHAR(50),
-        name VARCHAR(200),
-        price INT,
-        minimum_deposit INT,
-        minimum_installment INT,
-        CC VARCHAR(50),
-        link VARCHAR(100),
-        make VARCHAR(50),
-        model VARCHAR(50)
-    );
-
-    """
-    cursor.execute(SQL)
-    conn.commit()
-    conn.close()
-
-
-def create_pgsql_table_used_cars():
-    conn = psycopg2.connect(database='cars',user='postgres',password='Amr')
-    cursor=conn.cursor()
-    
-    SQL =f"""
-
-    CREATE TABLE IF NOT EXISTS used_cars(
-        id VARCHAR(50),
-        price INT,
-        phone_number VARCHAR(20),
-        installment INT,
-        deposit INT
-    );
-
-    """
-    cursor.execute(SQL)
-    conn.commit()
-    conn.close()
-
-
-
-def pgsql_add_column(table:str,column:str):
-    conn = psycopg2.connect(database='cars',user='postgres',password='Amr')
-    cursor=conn.cursor()
-    
-    SQL =f"""
-
-    ALTER TABLE {table}
-    ADD COLUMN IF NOT EXISTS {column} VARCHAR(50)  
-    ;
-
-    """
-    cursor.execute(SQL)
-    conn.commit()
-    conn.close()
-
-
-def read_pgsql_table(table):
-    conn = psycopg2.connect(database='cars',user='postgres',password='Amr')
-    cursor=conn.cursor()
-    
-    SQL =f"""
-
-    SELECT * FROM {table};
-    """
-    cursor.execute(SQL)
-    result = cursor.fetchall()
-    conn.close()
-    print(result)
-
-
-def len_pgsql_table(table):
-    conn = psycopg2.connect(database='cars',user='postgres',password='Amr')
-    cursor=conn.cursor()
-    
-    SQL =f"""
-
-    SELECT COUNT(*) FROM {table};
-    """
-    cursor.execute(SQL)
-    result = cursor.fetchall()
-    conn.close()
-    return result[0][0]
-
-
-def insert_pgsql_table(data):
-    conn = psycopg2.connect(database='cars',user='postgres',password='Amr')
-    cursor=conn.cursor()
-    values = tuple([data[column] for column in data])
-
-    SQL =f"""
-
-    INSERT INTO new_cars
-    VALUES {values} 
-    
-    ;
-    """
-    print('Values Inserted')
-    cursor.execute(SQL)
-    conn.commit()
-    conn.close()
-
-
-def insert_pgsql_table_used(table,data:dict):
-    conn = psycopg2.connect(database='cars',user='postgres',password='Amr')
-    cursor=conn.cursor()
-    columns = tuple(data.keys())
-    values = tuple(data.values())
-
-    SQL =f"""
-
-    INSERT INTO {table} 
-    ({','.join([column.strip().replace(' ','_') for column in columns])})
-    VALUES {values} 
-    
-    ;
-    """
-    print('Values Inserted')
-    cursor.execute(SQL)
-    conn.commit()
-    conn.close()
-
-
-def drop_pgsql_table(table):
-    conn = psycopg2.connect(database='cars',user='postgres',password='Amr')
-    cursor=conn.cursor()
-    
-    SQL =f"""
-
-    DROP TABLE {table}
-    ;
-    """
-    cursor.execute(SQL)
-    conn.commit()
-    conn.close()
-
-
-def scrape_new():
-    get_links_new()
-    for link in new_cars_links:
-        extract_car_details_new(get_car_detail(link))
-    pd.DataFrame(new_cars).to_csv('new_cars.csv')
-
-
+    logging.info(car['make']," ", car['model'])
+    sql.insert_pgsql_table_used('used_cars',car)
 
 if __name__ == '__main__':
-    #get_links_used()
-    #for link in used_cars_links:
-    #    extract_car_details_used(link)
-    # with open ('usedcars.txt','r') as f:
-    #     x = len(f.readlines())//40 +1
-    # get_links_used(x)
-    # create_pgsql_table_used_cars()
-    saved =len_pgsql_table('used_cars')
-    print(saved)
-    with open('usedcars.txt','r') as f:
-        for _ in range(saved+2):
-            next(f)
-        for link in f:
-            print(link)
-            extract_car_details_used(link.strip())
-    #pd.DataFrame(used_cars).to_csv('used_cars.csv',index=False)
+    sql.create_pgsql_table_new_cars_links()
+    sql.create_pgsql_table_used_cars_links()
+    get_links_new()
+    get_links_used()
+    extract_car_details_new(sql.read_link('new_cars_links',1))
+    extract_car_details_used(sql.read_link('used_cars_links',1))
---
+++ sql.py
@@ -0,0 +1,224 @@
+import psycopg2
+import logging
+import json
+
+
+with open('db_info.json','r') as f:
+    db_config:dict = json.load(f)
+
+database: str = db_config['database']
+user: str = db_config['user']
+password: str = db_config['password']
+
+def create_pgsql_table_new_cars()-> None:
+    conn = psycopg2.connect(database,user,password)
+    cursor = conn.cursor()
+    SQL =f"""
+    CREATE TABLE IF NOT EXISTS new_cars(
+        id VARCHAR(50),
+        name VARCHAR(200),
+        price INT,
+        minimum_deposit INT,
+        minimum_installment INT,
+        CC VARCHAR(50),
+        link VARCHAR(100),
+        make VARCHAR(50),
+        model VARCHAR(50)
+    );
+    """
+    cursor.execute(SQL)
+    conn.commit()
+    conn.close()
+
+
+def create_pgsql_table_used_cars() -> None:
+    conn = psycopg2.connect(database=database,user=user,password=password)
+    cursor = conn.cursor()
+    
+    SQL =f"""
+
+    CREATE TABLE IF NOT EXISTS used_cars(
+        id VARCHAR(50),
+        price INT,
+        phone_number VARCHAR(20),
+        installment INT,
+        deposit INT
+    );
+
+    """
+    cursor.execute(SQL)
+    conn.commit()
+    conn.close()
+
+
+
+def pgsql_add_column(table: str,column: str)-> None:
+    conn = psycopg2.connect(database=database,user=user,password=password)
+    cursor = conn.cursor()
+    
+    SQL =f"""
+
+    ALTER TABLE {table}
+    ADD COLUMN IF NOT EXISTS {column} VARCHAR(50)  
+    ;
+
+    """
+    cursor.execute(SQL)
+    conn.commit()
+    conn.close()
+
+
+def read_pgsql_table(table: str)-> None:
+    conn = psycopg2.connect(database=database,user=user,password=password)
+    cursor = conn.cursor()
+    
+    SQL =f"""
+
+    SELECT * FROM {table};
+    """
+    cursor.execute(SQL)
+    result = cursor.fetchall()
+    conn.close()
+    print(result)
+
+
+def len_pgsql_table(table: str) -> int:
+    conn = psycopg2.connect(database=database,user=user,password=password)
+    cursor = conn.cursor()
+    
+    SQL =f"""
+
+    select count(*) from {table};
+    """
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    conn.close()
+    return result[0][0]
+
+
+def insert_pgsql_table(data:dict)-> None:
+    conn = psycopg2.connect(database=database,user=user,password=password)
+    cursor=conn.cursor()
+    values = tuple([data[column] for column in data])
+    SQL =f"""
+    INSERT INTO new_cars
+    VALUES {values} 
+    ;
+    """
+    print('Values Inserted')
+    cursor.execute(SQL)
+    conn.commit()
+    conn.close()
+
+
+def insert_pgsql_table_used(table,data:dict):
+    conn = psycopg2.connect(database=database,user=user,password=password)
+    cursor=conn.cursor()
+    columns = tuple(data.keys())
+    values = tuple(data.values())
+
+    SQL =f"""
+    INSERT INTO {table} 
+    ({','.join([column.strip().replace(' ','_') for column in columns])})
+    VALUES {values} 
+    
+    ;
+    """
+    print('Values Inserted')
+    cursor.execute(SQL)
+    conn.commit()
+    conn.close()
+
+
+def drop_pgsql_table(table):
+    conn = psycopg2.connect(database=database,user=user,password=password)
+    cursor=conn.cursor()
+    
+    SQL =f"""
+
+    DROP TABLE {table}
+    ;
+    """
+    cursor.execute(SQL)
+    conn.commit()
+    conn.close()
+
+
+def create_pgsql_table_new_cars_links()-> None:
+    conn = psycopg2.connect(database=database,user=user,password=password)
+    cursor = conn.cursor()
+    SQL =f"""
+    CREATE TABLE IF NOT EXISTS new_cars_links(
+        id serial NOT NULL,
+        link VARCHAR(250) UNIQUE
+    );
+    """
+    cursor.execute(SQL)
+    conn.commit()
+    conn.close()
+
+
+def create_pgsql_table_used_cars_links()-> None:
+    conn = psycopg2.connect(database=database,user=user,password=password)
+    cursor = conn.cursor()
+    SQL =f"""
+    CREATE TABLE IF NOT EXISTS used_cars_links(
+        id serial NOT NULL,
+        link VARCHAR(250) UNIQUE
+    );
+    """
+    cursor.execute(SQL)
+    conn.commit()
+    conn.close()
+
+
+def insert_link(table_name: str,link: str)-> None:
+    conn = psycopg2.connect(database=database,user=user,password=password)
+    cursor=conn.cursor()
+    logging.info(f'added {link}')
+    SQL =f"""
+    INSERT INTO {table_name} (link)
+    VALUES ('{link}')
+    ;
+    """
+    try:
+        cursor.execute(SQL)
+        conn.commit()
+    except psycopg2.IntegrityError as e:
+        logging.error(e.pgcode)
+        pass
+    conn.close()
+
+
+def read_link(table: str,days: int)-> list[str]:
+    conn = psycopg2.connect(database=database,user=user,password=password)
+    cursor = conn.cursor()
+    
+    SQL =f"""
+
+    SELECT * FROM {table}
+    WHERE date > CURRENT_TIMESTAMP - INTERVAL '{days}day'
+    ;
+    """
+    cursor.execute(SQL)
+    result = cursor.fetchall()
+    conn.close()
+    return [entry[1] for entry in result]
+
+
+def read_links(table: str)-> list[str]:
+    conn = psycopg2.connect(database=database,user=user,password=password)
+    cursor = conn.cursor()
+    
+    SQL =f"""
+
+    SELECT * FROM {table}
+    ;
+    """
+    cursor.execute(SQL)
+    result = cursor.fetchall()
+    conn.close()
+    return [entry[1] for entry in result]
+
+if __name__ == '__main__':
+    print(read_links('used_cars_links'))
